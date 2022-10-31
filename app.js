6b875c600c1a7c07389d6b61366e3f38963d69fb
nick Fonseca <nick.fonseca79@hmail.com>
2022-10-30 22:10:48 -0700

    Variables

---
+++ app.js
@@ -0,0 +1,23 @@
+// var, let, const 
+// var name ='Bob Smith';
+// console.log(name);
+// var name = 'Fred Smith';
+// console.log(name);
+
+// //init var
+// var greetings;
+// console.log(greetings);
+// var greetings = 'HelloWorld';
+// console.log(greetings);
+
+// // letters, numbers, _, $
+// // Cannot start with a numbers
+
+// // Multi word variables
+// var firstName = 'John Smith'; //cammel case
+
+// let 
+let name ='Bob Smith';
+console.log(name);
+
+console.log(name);
